2c8e9064d3717f53f548943127b79b3e75172bce
Michael Millward <millwamich1@pulcinella.ecs.vuw.ac.nz>
2015-08-10 15:53:34 +1200

    added getColumnWidth

--- src/System/PhDTable.java
+++ src/System/PhDTable.java
@@ -37,6 +37,24 @@ public abstract class PhDTable {
 		this.headers = headers;
 	}
 
+	public int[] getColumnWidth(String[] headers){
+		int[] biggestWidth = new int[headers.length];
+		for(int i = 0; i<headers.length; i++){
+			biggestWidth[i] = -1;
+		}
+		// Go through all the students
+		for(int i = 0; i<students.size(); i++){
+			String[] current = students.get(i).getValues(headers);
+			// Now go through each header of the student and check what the length is
+			for(int j = 0; j<headers.length; j++){
+				if(headers[j].length() > biggestWidth[j]){
+					biggestWidth[j] = headers[j].length();
+				}
+			}
+		}
+		return biggestWidth;
+	}
+
 	public int getSizeOfStudents(){
 		return students.size();
 	}
