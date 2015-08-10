2056e76b2fed3804bf334227110754aa2e3d24f7
Michael Millward <millwamich1@pulcinella.ecs.vuw.ac.nz>
2015-08-10 13:52:11 +1200

    getters and setters added for PhDData

--- src/System/PhDData.java
+++ src/System/PhDData.java
@@ -8,30 +8,42 @@ public class PhDData {
 	private OtherSchoolsAtVUW otherSchoolsAtVUW;
 	private OtherUniversities otherUniversities;
 
-	public PhDData(
-			UnderExamination underExamination,
-			CurrentFullyRegistered currentFullyRegistered,
-			CurrentProvisionallyRegisteredStudents currentProvisionallyRegisteredStudents,
-			NotFullyAdmitted notFullyAdmitted,
-			OtherSchoolsAtVUW otherSchoolsAtVUW,
-			OtherUniversities otherUniversities) {
+	public UnderExamination getUnderExamination() {
+		return underExamination;
+	}
+	public void setUnderExamination(UnderExamination underExamination) {
 		this.underExamination = underExamination;
+	}
+	public CurrentFullyRegistered getCurrentFullyRegistered() {
+		return currentFullyRegistered;
+	}
+	public void setCurrentFullyRegistered(CurrentFullyRegistered currentFullyRegistered) {
 		this.currentFullyRegistered = currentFullyRegistered;
+	}
+	public CurrentProvisionallyRegisteredStudents getCurrentProvisionallyRegisteredStudents() {
+		return currentProvisionallyRegisteredStudents;
+	}
+	public void setCurrentProvisionallyRegisteredStudents(
+			CurrentProvisionallyRegisteredStudents currentProvisionallyRegisteredStudents) {
 		this.currentProvisionallyRegisteredStudents = currentProvisionallyRegisteredStudents;
+	}
+	public NotFullyAdmitted getNotFullyAdmitted() {
+		return notFullyAdmitted;
+	}
+	public void setNotFullyAdmitted(NotFullyAdmitted notFullyAdmitted) {
 		this.notFullyAdmitted = notFullyAdmitted;
-		this.otherSchoolsAtVUW = otherSchoolsAtVUW;
+	}
+	public OtherUniversities getOtherUniversities() {
+		return otherUniversities;
+	}
+	public void setOtherUniversities(OtherUniversities otherUniversities) {
 		this.otherUniversities = otherUniversities;
 	}
-
-	/* Construtor used for testing */
-	public PhDData(
-			UnderExamination underExamination,
-			CurrentFullyRegistered currentFullyRegistered,
-			CurrentProvisionallyRegisteredStudents currentProvisionallyRegisteredStudents,
-			NotFullyAdmitted notFullyAdmitted) {
-		this.underExamination = underExamination;
-		this.currentFullyRegistered = currentFullyRegistered;
-		this.currentProvisionallyRegisteredStudents = currentProvisionallyRegisteredStudents;
-		this.notFullyAdmitted = notFullyAdmitted;
+	public OtherSchoolsAtVUW getOtherSchoolsAtVUW() {
+		return otherSchoolsAtVUW;
 	}
+	public void setOtherSchoolsAtVUW(OtherSchoolsAtVUW otherSchoolsAtVUW) {
+		this.otherSchoolsAtVUW = otherSchoolsAtVUW;
+	}
+
 }
