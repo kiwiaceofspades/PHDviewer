579b5a446a68e629b4b3b40418e311788bd70c42
Matthew <sch93nz@hotmail.com>
2015-09-21 11:38:38 +1200

    A message because Matthew is too stupid to use vim

--- src/GUI/Table.java
+++ src/GUI/Table.java
@@ -1,7 +1,6 @@
 package GUI;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
