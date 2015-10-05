4134f2c51101cdd952ce1a64c56b6d360a2ed64f
Harry <ha_zm_an@hotmail.com>
2015-10-05 13:46:32 +1300

    Updated the licence as well added copyright to Main and UnitTest.

--- src/Testing/UnitTest.java
+++ src/Testing/UnitTest.java
@@ -1,3 +1,23 @@
+/**
+ *
+ * Copyright (C) 2015  Harry King
+ *
+ * This file is part of PHDViewer.
+ *
+ * PHDViewer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PHDViewer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package Testing;
 
 import static org.junit.Assert.*;
