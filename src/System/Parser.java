beb402af9ce4e9e07ce217f3addcb4bea290f15a
Harry <ha_zm_an@hotmail.com>
2015-10-12 12:22:31 +1300

    Update Parser.java
    Added in the copyright

--- src/System/Parser.java
+++ src/System/Parser.java
@@ -1,3 +1,23 @@
+/**
+ *
+ * Copyright (C) 2015  Shreyas Ramasubramanian
+ *
+ * This file is part of PHDViewer.
+ *
+ * PHDViewer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PHDViewer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package System;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,7 +28,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-// Author: Shreyas (ramasushre)
+/**
+ * @Author: Shreyas Ramasubramanian
+ */
 public class Parser {
 
 	// Fields used for testing
